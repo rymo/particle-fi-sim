992426e8217cc6695bf9e2d2066350a21a8a5417
Ryan Mott <ryan.mott@vml.com>
2016-12-02 16:58:18 -0600

    can't have parallel `setup()` functions, this becomes a demo only.

---
+++ firmware/examples/keep-alive.ino
@@ -0,0 +1,5 @@
+#include "project-fi-sim/project-fi-sim.h"
+
+void setup() {
+  Particle.keepAlive(240);
+}
--- firmware/project-fi-sim.cpp
+++ firmware/project-fi-sim.cpp
@@ -1,8 +1,3 @@
 #include "project-fi-sim.h"
-#include "application.h"
 #include "cellular_hal.h"
 STARTUP(cellular_credentials_set("h2g2", "", "", NULL));
-
-void setup() {
-  Particle.keepAlive(240);
-}
